e89e782c7b632d76dcd095adeee5e3f347bdabac
IT21440236 <it21440236@my.sliit.lk>
2023-07-30 15:54:58 +0530

    event add

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,6 @@ import Login from "./components/Login";
 import Events from "./components/Events";
 
 import Home from "./components/Home";
-<<<<<<< HEAD
-import NavBar from "./components/NavBar";
-=======
-import Ticket from "./components/Tickets";
->>>>>>> 87cdc6bce6075f786ecf10ef499a3cb94079f65b
 
 function App() {
   return (
@@ -18,11 +13,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/events" element={<Events />} />
-=======
-          <Route path = "/tickets" element={<Ticket/>}  />
->>>>>>> 87cdc6bce6075f786ecf10ef499a3cb94079f65b
         </Routes>
       </BrowserRouter>
     </div>
