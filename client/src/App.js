217f986feb95a651b7c41ca7e6b7ede89aac652e
IT21440236 <it21440236@my.sliit.lk>
2023-07-30 15:48:25 +0530

    event add

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Events from "./components/Events";
 
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Login />} />
+          <Route path="/events" element={<Events />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,119 +1,172 @@
-// // import projImg1 from "../assets/img/project-img1.png";
-// // import projImg2 from "../assets/img/project-img2.png";
-// // import projImg3 from "../assets/img/project-img3.png";
-// import colorSharp2 from "../assets/img/color-sharp2.png";
-// import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
-// import ProjectCard from "./ProjectCard";
-// import "animate.css";
-// import TrackVisibility from "react-on-screen";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Container, Paper } from "@mui/material";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
 
-// const Events = () => {
-//   const projects = [
-//     {
-//       title: "SOUTHERN AGRO SERVE (Pvt) Ltd",
-//       description:
-//         "BUSINESS MANAGEMENT SYSTEM FOR SOUTHERN AGRO SERVE (Pvt) Ltd",
-//       imgUrl:
-//         "https://lvivity.com/wp-content/uploads/2020/04/iot-in-agriculture.jpg",
-//       projectURL: "https://github.com/SLIITITP/ITP_WE_B01_TW_14",
-//     },
-//     {
-//       title: "FinTrack",
-//       description:
-//         "The FinTrack mobile application is made to assist people in effectively managing their finances",
-//       imgUrl:
-//         "https://i.pinimg.com/originals/49/44/fd/4944fd56b523dd0a1ca27334a6005653.png",
-//       projectURL: "https://github.com/IT21440236/FinTrack",
-//     },
-//     {
-//       title: "Online Pet Care System",
-//       description:
-//         "Web Application to cater the services required by pets and their owners.",
-//       imgUrl:
-//         "https://d3544la1u8djza.cloudfront.net/APHI/Blog/2022/04-18/woman+and+dog+having+an+online+consultation+with+a+veterinarian-min.jpg",
-//       projectURL: "https://github.com/IT21440236/Online-Pet-Care-System",
-//     },
-//     {
-//       title: "Online Grocery Ordering System",
-//       description: "Web Application to cater the services required customers.",
-//       imgUrl:
-//         "https://www.lifewire.com/thmb/QSOMHSqmmeKBioN9SK0j4TEiM8U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Primary-Image-he-11-best-online-grocery-shopping-sites-in-2023-3482646-92bbb19773c84315a1f78c7ecf628bce.jpg",
-//       projectURL:
-//         "https://github.com/IT21440236/Online-Grocery-Ordering-System---Y2S1",
-//     },
-//     {
-//       title: "TheCodingTrio",
-//       description: "We Build Pat Projects",
-//       imgUrl:
-//         "https://img.freepik.com/free-vector/teenager-boy-cozy-room-attic-interior-cartoon-vector-with-roof-window_33099-1327.jpg?w=2000",
-//       projectURL: "https://github.com/TheCodingTrio",
-//     },
-//   ];
+export default function Events() {
+  const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [eventCode, setEventCode] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [venue, setVenue] = useState("");
+  const [description, setDescription] = useState("");
+  const [organizer, setOrganizer] = useState("");
+  const [category, setCategory] = useState("");
+  const [guestCount, setGuestCount] = useState("");
+  const [status, setStatus] = useState([]);
 
-//   return (
-//     <section className="project" id="projects">
-//       {/* id attribute is wht navigates you to the relevant section when u click on the navbar links. The href="#projects" text must be equal to the id attribute. */}
-//       <Container>
-//         <Row>
-//           <Col>
-//             <TrackVisibility>
-//               {(
-//                 { isVisible } //Is visible is from TrackVisibility.
-//               ) => (
-//                 //not from react.(make sure the remove the import that automatically comes when u type in isVisible)
-//                 <div
-//                   className={
-//                     isVisible ? "animate__animated animate__slideInUp" : ""
-//                   } //These classnames u can obtain from https://animate.style/
-//                 >
-//                   <h2>Projects</h2>
-//                   <p>
-//                     Explore my projects, where innovation meets functionality.
-//                     From responsive designs to seamless user experiences.
-//                   </p>
-//                 </div>
-//               )}
-//             </TrackVisibility>
-//             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-//               <Nav
-//                 variant="pills"
-//                 className="nav-pills mb-5 justify-content-center align-items-center"
-//                 id="pills-tab"
-//               >
-//                 <Nav.Item>
-//                   <Nav.Link eventKey="first">Tab 1</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item>
-//                   <Nav.Link eventKey="second">Tab 2</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item>
-//                   <Nav.Link eventKey="third">Tab 3</Nav.Link>
-//                 </Nav.Item>
-//               </Nav>
-//               <Tab.Content>
-//                 <Tab.Pane eventKey="first">
-//                   <Row>
-//                     {projects.map((project, index) => {
-//                       return <ProjectCard key={index} {...project} />;
-//                     })}
-//                   </Row>
-//                 </Tab.Pane>
-//                 <Tab.Pane eventKey="second">
-//                   Await for more projects...
-//                 </Tab.Pane>
-//                 <Tab.Pane eventKey="third">Await for more projects...</Tab.Pane>
-//               </Tab.Content>
-//             </Tab.Container>
-//           </Col>
-//         </Row>
-//       </Container>
-//       <img
-//         className="background-image-right"
-//         src={colorSharp2}
-//         alt="colorSharp2"
-//       />
-//     </section>
-//   );
-// };
+  const handleClick = (e) => {
+    e.preventDefault();
+    const event = {
+      eventCode,
+      eventName,
+      date,
+      time,
+      venue,
+      description,
+      organizer,
+      category,
+      guestCount,
+      status,
+    };
+    console.log(event);
+    fetch("http://localhost:8081/api/v1/event/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(event),
+      //stringify() method converts a JavaScript object or value to a JSON string,
+    }).then(() => {
+      console.log("New Event Added");
+    });
+  };
 
-// export default Events;
+  //   useEffect(() => {
+  //     fetch("http://localhost:8080/api/v1/event/get")
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         setStudents(result);
+  //       });
+  //   }, []);
+
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: "blue" }}>
+          <u>Add Event</u>
+        </h1>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Event Code"
+            variant="outlined"
+            fullWidth
+            value={eventCode}
+            onChange={(e) => setEventCode(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Event Name"
+            variant="outlined"
+            fullWidth
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Date"
+            variant="outlined"
+            fullWidth
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Time"
+            variant="outlined"
+            fullWidth
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Venue"
+            variant="outlined"
+            fullWidth
+            value={venue}
+            onChange={(e) => setVenue(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+            fullWidth
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Organizer"
+            variant="outlined"
+            fullWidth
+            value={organizer}
+            onChange={(e) => setOrganizer(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Category"
+            variant="outlined"
+            fullWidth
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Guest Count"
+            variant="outlined"
+            fullWidth
+            value={guestCount}
+            onChange={(e) => setGuestCount(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Status"
+            variant="outlined"
+            fullWidth
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          />
+          <Button variant="contained" color="success" onClick={handleClick}>
+            Submit
+          </Button>
+        </Box>
+      </Paper>
+      {/* <h1>Students</h1> */}
+
+      {/* <Paper elevation={3} style={paperStyle}>
+        {students.map((student) => (
+          <Paper
+            elevation={6}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}
+          >
+            Id:{student.id}
+            <br />
+            Name:{student.name}
+            <br />
+            Address:{student.address}
+          </Paper>
+        ))}
+      </Paper> */}
+    </Container>
+  );
+}
